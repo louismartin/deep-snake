1fcb4e131fe5bb748732599c6040506e5301e88d
Louis MARTIN <louisrtm@gmail.com>
2016-12-30 15:49:59 +0100

    Rename policy gradient file

--- snake/demo_policy.py
+++ snake/demo_policy.py
@@ -1,6 +1,6 @@
 import time
 
-from policy_gradient_fc import train, test
+from policy_gradient import train, test
 from models.model_base import FullyConnected
 from snake import Snake
 
@@ -12,7 +12,7 @@ n_input = 2 * snake.grid_size * snake.grid_size
 model = FullyConnected(n_input=n_input, n_hidden=200, n_classes=4)
 
 start_time = time.time()
-train(model, snake, n_batch=100, n_iterations=1000, gamma = .95, learning_rate = 0.001)
+train(model, snake, n_batch=100, n_iterations=1000, gamma=.95, learning_rate=0.001)
 end_time = time.time()
 print('Elapsed %.2f seconds' % (end_time - start_time))
 
