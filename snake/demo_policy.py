9672ae9a1b04c9e545dd744308511a98a5b9494d
Louis MARTIN <louisrtm@gmail.com>
2016-12-30 18:28:20 +0100

    First working naive convolutional neural network

--- snake/demo_policy.py
+++ snake/demo_policy.py
@@ -1,7 +1,7 @@
 import time
 
 from policy_gradient import train, test
-from models.model_base import FullyConnected
+from models.model_base import FullyConnected, ConvNet
 from snake import Snake
 
 # load THE SNAKE
@@ -9,10 +9,11 @@ snake = Snake(rewards={'nothing':-1, 'bitten':-10, 'out':-10, 'food':500})
 
 # initialize the model
 n_input = 2 * snake.grid_size * snake.grid_size
-model = FullyConnected(n_input=n_input, n_hidden=200, n_classes=4)
+#model = FullyConnected(n_input=n_input, n_hidden=200, n_classes=4)
+model = ConvNet(grid_size=snake.grid_size, n_frames=2, n_classes=4)
 
 start_time = time.time()
-train(model, snake, batch_size=100, n_iterations=1000, gamma=.95, learning_rate=0.001)
+train(model, snake, batch_size=200, n_iterations=2000, gamma=.95, learning_rate=0.001)
 end_time = time.time()
 print('Elapsed %.2f seconds' % (end_time - start_time))
 
